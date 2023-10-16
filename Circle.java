dcdceb3d3eea65bc81706f9d8ad9ecd3f15781d6
Oh <eric0h@naver.com>
2023-10-17 00:46:29 +0900

    book과 Circle을 커밋

---
+++ Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    int radius;
+    public Circle() {this.radius = 1;}
+    public Circle(int radius){
+        this.radius = radius;
+    }
+    double getArea(){
+        return 3.14*radius*radius;
+    }
+}
+
